9c0dcd10bb45c7eb301fab23f2efbb54a53387bf
Surdu Constantin <tyk1995x@gmail.com>
2026-05-19 12:25:56 +0300

    chore(main): release 0.1.12

--- cmd/kubekit/main.go
+++ cmd/kubekit/main.go
@@ -12,7 +12,7 @@ import (
 
 // Version is set at build time via -ldflags. The string after the `=` is
 // updated automatically by release-please from .release-please-manifest.json.
-var Version = "0.2.0-dev" // x-release-please-version
+var Version = "0.1.12" // x-release-please-version
 
 func main() {
 	if len(os.Args) > 1 {
